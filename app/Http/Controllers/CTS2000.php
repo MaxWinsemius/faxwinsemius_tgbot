4ceccac0e79ba97ed75c75280d34c082edc7529d
Max@Blackpearl <maxwinsemius@me.com>
2021-02-16 20:51:09 +0100

    sha244->sha225

--- app/Http/Controllers/CTS2000.php
+++ app/Http/Controllers/CTS2000.php
@@ -23,7 +23,7 @@ class CTS2000 extends Printer
 
     static public function printText(string $string)
     {
-        $file = '/messages/' . hash('sha244', $string . now()) . '.data';
+        $file = '/messages/' . hash('sha224', $string . now()) . '.data';
         Storage::disk('local')->put($file, $string);
 
         return CTS2000::printFile($file);
