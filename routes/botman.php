680a21a70b5e3fd8ca4d018bd59f409d36180404
Max@Blackpearl <maxwinsemius@me.com>
2021-03-03 22:28:18 +0100

    Added /stats command

--- routes/botman.php
+++ routes/botman.php
@@ -9,6 +9,7 @@ $botman = resolve('botman');
 $botman->hears('/start', BotManController::class.'@start');
 $botman->hears('/help', BotManController::class.'@help');
 $botman->hears('/status', BotUserController::class.'@status');
+$botman->hears('/stats', BotUserController::class.'@status');
 $botman->hears('/license', BotUserController::class.'@register');
 
 $botman->group(['middleware' => new MatchPrintingAllowed()], function ($botman) {
